4ce6fc8f5f4d05c7a5448dd3323de8eacd28cffe
Oleksii Patsiuk <patsyuck@ukr.net>
2021-06-28 13:13:54 +0300

    Додані маршрутизація та перенаправлення

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
 import { Cast } from './components/Cast';
 import { HomePage } from './components/HomePage';
@@ -5,16 +12,53 @@ import { MovieDetailsPage } from './components/MovieDetailsPage';
 import { MoviesPage } from './components/MoviesPage';
 import { Reviews } from './components/Reviews';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage />
-      <MoviesPage />
-      <MovieDetailsPage />
-      <Cast />
-      <Reviews />
-    </div>
-  );
+const API_KEY = '467d5570ee3cf60e90370c1786a53e75';
+const endpointPopularFilms = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
+
+export class App extends Component {
+  state = {
+    loading: false,
+  };
+
+  async handleRequest(endpoint) {
+    this.setState({ loading: true });
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      console.log(data.results);
+      /*const cards = data.hits.map(hit => ({
+        id: hit.id,
+        image: hit.webformatURL,
+        bigImage: hit.largeImageURL,
+      }));
+      this.setState({ cards: [...this.state.cards, ...cards] });*/
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  async componentDidMount() {
+    this.handleRequest(endpointPopularFilms);
+  }
+
+  render() {
+    return (
+      /*<div className="App">*/
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/movies" exact component={MoviesPage} />
+          <Route path="/movies/:movieId" exact component={MovieDetailsPage} />
+          <Route path="/movies/:movieId/cast" exact component={Cast} />
+          <Route path="/movies/:movieId/reviews" exact component={Reviews} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+      /*</div>*/
+    );
+  }
 }
 
 export default App;
