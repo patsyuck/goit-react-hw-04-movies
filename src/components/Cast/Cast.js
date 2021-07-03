1d0049604764e504adb078b2f179bde9ed106557
Oleksii Patsiuk <patsyuck@ukr.net>
2021-07-03 22:19:48 +0300

    Додав рецензії

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,24 +1,12 @@
 import { Component } from 'react';
 import { endpointFilmInfo, tailFilmActors } from '../API';
 
-const API_KEY = '467d5570ee3cf60e90370c1786a53e75';
-
 export class Cast extends Component {
   state = {
     actors: [],
   };
 
   async componentDidMount() {
-    try {
-      const resp = await fetch(
-        `https://api.themoviedb.org/3/configuration?api_key=${API_KEY}`,
-      );
-      const config = await resp.json();
-      console.log(config);
-    } catch (error) {
-      console.error(error);
-    }
-
     try {
       const response = await fetch(
         endpointFilmInfo +
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,3 +1,42 @@
-export function Reviews() {
-  return <div>Reviews</div>;
+import { Component } from 'react';
+import { endpointFilmInfo, tailFilmReviews } from '../API';
+
+export class Reviews extends Component {
+  state = {
+    reviews: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        endpointFilmInfo +
+          `${this.props.match.params.movieId}` +
+          tailFilmReviews,
+      );
+      const data = await response.json();
+      const reviews = data.results.map(review => ({
+        author: review.author,
+        content: review.content,
+      }));
+      this.setState({ reviews: reviews });
+      console.log(this.state);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.reviews.map(review => (
+            <li>
+              <p>Author: {review.author}</p>
+              <p>{review.content}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
